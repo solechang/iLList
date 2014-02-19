83a3e2fbc4ff26d65a1e81468baeeb8733a7ce56
Jake <changwoo92@gmail.com>
2014-02-19 13:28:17 -0800

    Adding iLList

---
+++ iLList/iLList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iLList
+//
+//  Created by Jake Choi on 2/19/14.
+//  Copyright (c) 2014 iLList. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ILLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ILLAppDelegate class]));
+    }
+}
