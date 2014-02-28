33a0b39f6a19b9c04be419f81121937955fba33e
Jake <changwoo92@gmail.com>
2014-02-28 11:07:51 -0800

    Added LoginViewController

---
+++ iLList/iLList/ILLLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  ILLLoginViewController.m
+//  iLList
+//
+//  Created by Jake Choi on 2/28/14.
+//  Copyright (c) 2014 iLList. All rights reserved.
+//
+
+#import "ILLLoginViewController.h"
+
+@interface ILLLoginViewController ()
+
+@end
+
+@implementation ILLLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
