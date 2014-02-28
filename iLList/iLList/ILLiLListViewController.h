6d2a0ad253008aa335c482f82c0e8a3f78ccb82d
Jake <changwoo92@gmail.com>
2014-02-28 10:49:07 -0800

    Added in tableview

---
+++ iLList/iLList/ILLiLListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ILLiLListViewController.h
+//  iLList
+//
+//  Created by Jake Choi on 2/28/14.
+//  Copyright (c) 2014 iLList. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ILLiLListViewController : UITableViewController
+
+@end
